7fb014e1842747aff807fc18910f7bc6eb413406
Thoai <Trandamthoai2002@gmail.com>
2024-03-04 23:09:35 +0700

    thong tin chi tiết

--- src/App.js
+++ src/App.js
@@ -3,6 +3,22 @@ import { Layout, Menu, Modal, Input, Button } from 'antd';
 import 'antd/dist/reset.css';
 //import { Steps } from 'antd';
 //layout
+import { CopyOutlined } from '@ant-design/icons';
+import {
+  AutoComplete,
+  
+  Cascader,
+  DatePicker,
+  
+  InputNumber,
+  Select,
+  Space,
+  TimePicker,
+  Tooltip,
+  TreeSelect,
+} from 'antd';
+
+
 //import type { MenuProps } from 'antd';
 //import { LaptopOutlined, NotificationOutlined, UserOutlined } from '@ant-design/icons';
 
@@ -11,6 +27,9 @@ import 'antd/dist/reset.css';
 import App2 from './App2';
 const { Header, Content, Footer } = Layout;
 //const { Header, Content, Footer } = Layout;
+const { Option } = Select;
+const { TreeNode } = TreeSelect;
+
 ///////////////*
 
 /*
@@ -134,13 +153,196 @@ const App = () => {
 
 
             {/* Thêm nội dung giới thiệu */}
+            <Space direction="vertical">
+    <Space.Compact block>
+      <Input style={{ width: '20%' }} defaultValue="039" />
+      <Input style={{ width: '30%' }} defaultValue="4250578" />
+    </Space.Compact>
+    <Space.Compact block size="small">
+      <Input style={{ width: 'calc(100% - 200px)' }} defaultValue="https://ant.design" />
+      <Button type="primary">Submit</Button>
+    </Space.Compact>
+    <Space.Compact block>
+      <Input style={{ width: 'calc(100% - 200px)' }} defaultValue="https://ant.design" />
+      <Button type="primary">Submit</Button>
+    </Space.Compact>
+    <Space.Compact block>
+      <Input
+        style={{ width: 'calc(100% - 200px)' }}
+        defaultValue="git@github.com:ant-design/ant-design.git"
+      />
+      <Tooltip title="copy git url">
+        <Button icon={<CopyOutlined />} />
+      </Tooltip>
+    </Space.Compact>
+    <Space.Compact block>
+      <Select defaultValue="Zhejiang" allowClear>
+        <Option value="Zhejiang">THOẠI 1</Option>
+        <Option value="Jiangsu">THOẠI 2</Option>
+      </Select>
+      <Input style={{ width: '50%' }} defaultValue="Xihu District, Hangzhou" />
+    </Space.Compact>
+    <Space.Compact block>
+      <Select allowClear mode="multiple" defaultValue="Zhejianggggg" style={{ width: '50%' }}>
+        <Option value="Zhejianggggg">Zhejianggggg</Option>
+        <Option value="Jiangsu">Jiangsu</Option>
+      </Select>
+      <Input style={{ width: '50%' }} defaultValue="Xihu District, Hangzhou" />
+    </Space.Compact>
+    <Space.Compact block>
+      <Input.Search style={{ width: '30%' }} defaultValue="0571" />
+      <Input.Search allowClear style={{ width: '50%' }} defaultValue="26888888" />
+      <Input.Search style={{ width: '20%' }} defaultValue="+1" />
+    </Space.Compact>
+    <Space.Compact block>
+      <Select defaultValue="Option1">
+        <Option value="Option1">Option1</Option>
+        <Option value="Option2">Option2</Option>
+      </Select>
+      <Input style={{ width: '50%' }} defaultValue="input content" />
+      <InputNumber defaultValue={12} />
+    </Space.Compact>
+    <Space.Compact block>
+      <Input style={{ width: '50%' }} defaultValue="input content" />
+      <DatePicker style={{ width: '50%' }} />
+    </Space.Compact>
+    <Space.Compact block>
+      <DatePicker.RangePicker style={{ width: '70%' }} />
+      <Input style={{ width: '30%' }} defaultValue="input content" />
+      <Button type="primary">CHỌN</Button>
+    </Space.Compact>
+    <Space.Compact block>
+      <Input style={{ width: '30%' }} defaultValue="input content" />
+      <DatePicker.RangePicker style={{ width: '70%' }} />
+    </Space.Compact>
+    <Space.Compact block>
+      <Select defaultValue="Option1-1">
+        <Option value="Option1-1">Option1-1</Option>
+        <Option value="Option1-2">Option1-2</Option>
+      </Select>
+      <Select defaultValue="Option2-2">
+        <Option value="Option2-1">Option2-1</Option>
+        <Option value="Option2-2">Option2-2</Option>
+      </Select>
+    </Space.Compact>
+    <Space.Compact block>
+      <Select defaultValue="1">
+        <Option value="1">Between</Option>
+        <Option value="2">Except</Option>
+      </Select>
+      <Input style={{ width: 100, textAlign: 'center' }} placeholder="Minimum" />
+      <Input
+        className="site-input-split"
+        style={{
+          width: 30,
+          borderLeft: 0,
+          borderRight: 0,
+          pointerEvents: 'none',
+        }}
+        placeholder="~"
+        disabled
+      />
+      <Input
+        className="site-input-right"
+        style={{
+          width: 100,
+          textAlign: 'center',
+        }}
+        placeholder="Maximum"
+      />
+    </Space.Compact>
+    <Space.Compact block>
+      <Select defaultValue="Sign Up" style={{ width: '30%' }}>
+        <Option value="Sign Up">Sign Up</Option>
+        <Option value="Sign In">Sign In</Option>
+      </Select>
+      <AutoComplete
+        style={{ width: '70%' }}
+        placeholder="Email"
+        options={[{ value: 'text 1' }, { value: 'text 2' }]}
+      />
+    </Space.Compact>
+    <Space.Compact block>
+      <TimePicker style={{ width: '70%' }} />
+      <Cascader
+        style={{ width: '70%' }}
+        options={[
+          {
+            value: 'Sóc trăng',
+            label: 'Sóc Trăng',
+            children: [
+              {
+                value: 'hangzhou',
+                label: 'Ngã Năm',
+                children: [
+                  {
+                    value: 'xihu',
+                    label: 'Nhà tau',
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            value: 'jiangsu',
+            label: 'Hồ Chí Minh',
+            children: [
+              {
+                value: 'nanjing',
+                label: 'Bình Thạnh',
+                children: [
+                  {
+                    value: 'zhonghuamen',
+                    label: 'Cầu Bình Lợi',
+                  },
+                ],
+              },
+            ],
+          },
+        ]}
+        placeholder="Select Address"
+      />
+    </Space.Compact>
+    <Space.Compact block>
+      <TimePicker.RangePicker />
+      <TreeSelect
+        showSearch
+        style={{ width: '60%' }}
+        value="leaf1"
+        dropdownStyle={{ maxHeight: 400, overflow: 'auto' }}
+        placeholder="Please select"
+        allowClear
+        treeDefaultExpandAll
+        onChange={() => {}}
+      >
+        <TreeNode value="parent 1" title="parent 1">
+          <TreeNode value="parent 1-0" title="parent 1-0">
+            <TreeNode value="leaf1" title="leaf1" />
+            <TreeNode value="leaf2" title="leaf2" />
+          </TreeNode>
+          <TreeNode value="parent 1-1" title="parent 1-1">
+            <TreeNode value="leaf3" title={<b style={{ color: '#08c' }}>leaf3</b>} />
+          </TreeNode>
+        </TreeNode>
+      </TreeSelect>
+      <Button type="primary">Submit</Button>
+    </Space.Compact>
+    <Space.Compact>
+      <Input placeholder="input here" />
+      <InputNumber placeholder="another input" addonBefore="$" />
+      <InputNumber placeholder="another input" addonAfter="$" />
+    </Space.Compact>
+  </Space>
           </div>
+          
+          
+          
         )}
 
 
       </Content>
       {
-      <Footer style={{ position: 'fixed', bottom: 0, width: '100%', textAlign: 'center' }}>
+      <Footer style={{ position: 'fixed',bottom: 0, width: '100%', textAlign: 'center' }}>
         Bản quyền ©2024. Trang độc quyền của Thoại. All rights reserved.
       </Footer>
       }
